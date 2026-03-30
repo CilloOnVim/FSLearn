3b00fa54e3fdd8731db8604917d40a5701e0162d
CilloJoe <joelinux2025@gmail.com>
2026-03-30 16:59:41 +0800

    fix: configure whitenoise to serve production CSS

--- fslearn_main/settings.py
+++ fslearn_main/settings.py
@@ -33,7 +33,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 SECRET_KEY = "django-insecure-902t&hqz#-&_$#r=4+%@lb!6aaj_(*jvxtrgp85bxla48_ebrk"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('ENVIRONMENT') == 'development'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -99,7 +100,7 @@ if os.environ.get('ENVIRONMENT') == 'production':
             "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
         },
         "staticfiles": {
-            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage", # <-- THIS IS THE FIX
         },
     }
 else:
